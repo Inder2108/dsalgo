7233bbee8d553f5865454298b8b052b75ae74306
Inder2108 <Inder2108@users.noreply.github.com>
2022-07-08 20:53:31 +0530

    1. Created Stack Class

---
+++ ds/_1_Stack.java
@@ -0,0 +1,27 @@
+package datastructures;
+
+public class _1_Stack {
+
+    private int size;
+
+    _1_Stack() {
+        super();
+        this.size = 0;
+    }
+
+    // push method
+    public void push(String item) {
+
+    }
+
+    // pop method
+    public String pop() {
+        String s = "String";
+        return s;
+    }
+
+    // isEmpty method
+    public boolean isEmpty() {
+        return true;
+    }
+}
