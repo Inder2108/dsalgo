bc33a7590f283268611c9d48105d254bcac21455
Inder2108 <Inder2108@users.noreply.github.com>
2022-07-08 21:54:19 +0530

    LinkedList Implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dsalgo
+## 1. LinkedList
+## 2. Stack implementation using Linked List
---
+++ ds/_1_LinkedList.java
@@ -0,0 +1,32 @@
+package datastructures;
+
+/*
+ * Push and Pop are always at the begining of the List
+ * Performance: This ensures constant time in worst case
+*/
+public class _1_LinkedList {
+
+    private Class Node {
+        String item;
+        Node next;
+    }
+
+    private Node head = null;
+
+    public void push(String item) {
+        Node oldHead = this.head;
+        this.head = new Node();
+        this.head.item = item;
+        this.head.next = oldHead;
+    }
+
+    public String pop() {
+        String item = this.head.item;
+        this.head = this.head.next; 
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+}
