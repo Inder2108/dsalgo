eb07bc682027996d7e93ebc88774d0620393f5d8
Inder2108 <Inder2108@users.noreply.github.com>
2022-08-05 21:54:30 +0530

    Added validation array for testing

--- questions/_1_Arrays/_1_Pick_From_Both_Sides.java
+++ questions/_1_Arrays/_1_Pick_From_Both_Sides.java
@@ -32,7 +32,12 @@ public class _1_Pick_From_Both_Sides {
     }
 
     public static void main (String[] args) {
-        
+        int[] testArray = [ -533, -666, -500, 169, 724, 478, 358, -38, -536, 705, -855, 281, -173, 961, -509, -5, 942, -173, 436, -609, -396, 902, -847, -708, -618, 421, -284, 718, 895, 447, 726, -229, 538, 869, 912, 667, -701, 35, 894, -297, 811, 322, -667, 673, -336, 141, 711, -747, -132, 547, 644, -338, -243, -963, -141, -277, 741, 529, -222, -684, 35 ];
+        int count = 48;
+        int result = 6253;
+
+        System.out.println("Expected Result: " + result);
+        System.out.println("Your Result: " + solve(testArray, count)); // Should return 6253
     }
 
 }
